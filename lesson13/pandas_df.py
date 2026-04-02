99cc41b466e76c0e2348a22cc0968608298bff1f
unknown <Student@DIGITALSCHOOL.LOCAL>
2026-04-02 17:37:29 +0200

    commit

---
+++ lesson13/pandas_df.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data = {'Name':['Egzon', 'Liron', 'Melina'],
+        'Age' : [17,18,19],
+        'City' : ['Fush Kosove', 'Presheve', 'Prishtine']}
+
+df = pd.DataFrame(data)
+print(df)
+
---
+++ lesson13/pandas_series.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+from lesson13.array_manipulation import total_sum
+
+product = ['Apples', 'Bananas', 'Oranges', 'Grapes', 'Pineapples']
+
+sales = [150,200,180,90,60]
+
+sales_series = pd.Series(sales, index=product)
+
+print(sales_series)
+
+print(sales_series['Grapes'])
+
+total_sales = sales_series.sum()
+print(total_sales)
+
+best_selling_product = sales_series.idxmax()
+print(f"Best selling product: {best_selling_product}")
