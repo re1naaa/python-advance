ca4d9a823ab190aed9b2e3aa1d11346584ac51d0
erblinabehluli <Reinabeadini@gmail.com>
2026-05-26 17:41:26 +0200

    c

---
+++ lesson24/join.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+
+conn= sqlite3.connect('example.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS students(
+    student_id INTEGER PRIMARY KEY,
+    name TEXT
+)
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS courses(
+    course_id INTEGER PRIMARY KEY,
+    course_name TEXT,
+    student_id INTEGER,
+    FOREIGN KEY (student_id) REFERENCES students(student_id)
+)
+''')
+
+cursor.execute("INSERT INTO students (name) VALUES ('Festa')")
+cursor.execute("INSERT INTO students (name) VALUES ('Melina')")
+
+cursor.execute("INSERT INTO courses (course_name, student_id) VALUES ('Math' , 1)")
+cursor.execute("INSERT INTO courses (course_name, student_id) VALUES ('Science' , 1)")
+cursor.execute("INSERT INTO courses (course_name, student_id) VALUES ('Art' , 1)")
+
+conn.commit()
+
+cursor.execute('''
+     SELECT students.name, courses.course_name
+     from students
+     JOIN courses on students.student_id = courses.student_id
+''')
+
+rows = cursor.fetchall()
+for row in rows:
+    print(f"Students: {row[0]}, Course: {row[1]}")
+
+conn.close()
+
